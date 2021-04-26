81cee874008ac52071a65e596ff7236260946413
빈준호 <shycompany@naver.com>
2021-04-26 13:49:49 +0900

    feat: 백엔드 몽고DB 주석화, Hello World로 우선 빌드

--- backend/roufarm/src/main/java/com/c105/roufarm/controller/MainController.java
+++ backend/roufarm/src/main/java/com/c105/roufarm/controller/MainController.java
@@ -1,11 +1,12 @@
 package com.c105.roufarm.controller;
-import com.c105.roufarm.repository.PersonMongoDBRepository;
+// import com.c105.roufarm.model.Person;
+// import com.c105.roufarm.repository.PersonMongoDBRepository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class MainController {
 
-      @Autowired
-      private PersonMongoDBRepository personMongoDBRepository;
+      // @Autowired
+      // private PersonMongoDBRepository personMongoDBRepository;
 
-      @GetMapping("/{name}")
-      public ResponseEntity<String> Test(@PathVariable String name){
-            System.out.println(personMongoDBRepository.findAll());
-            return new ResponseEntity<String>(personMongoDBRepository.findAll().toString(),HttpStatus.OK);
+      @GetMapping("")
+      public String Helloworld(){
+            return "Hello, World!";
       }
+
+      // @GetMapping("/{name}")
+      // public ResponseEntity<String> Test(@PathVariable String name){
+      //       Person p = new Person();
+      //       p.setId("123");
+      //       p.setJob("good");
+      //       p.setName(name);
+      //       personMongoDBRepository.save(p);
+      //       return new ResponseEntity<String>(personMongoDBRepository.findById("500").toString(),HttpStatus.OK);
+      // }
 }
