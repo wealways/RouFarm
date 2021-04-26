bbf52e7954970c1ae52fc99a375f9f013d48c0c7
leekangrim01 <leekangrim01@naver.com>
2021-04-26 22:13:09 +0900

    java8 backend files

--- backend/roufarm/src/main/java/com/c105/roufarm/controller/MainController.java
+++ backend/roufarm/src/main/java/com/c105/roufarm/controller/MainController.java
@@ -1,12 +1,6 @@
 package com.c105.roufarm.controller;
-// import com.c105.roufarm.model.Person;
-// import com.c105.roufarm.repository.PersonMongoDBRepository;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,21 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class MainController {
 
-      // @Autowired
-      // private PersonMongoDBRepository personMongoDBRepository;
-
       @GetMapping("")
-      public String Helloworld(){
-            return "Hello, World!";
+      public String Test() {
+            return "Hello";
       }
-
-      // @GetMapping("/{name}")
-      // public ResponseEntity<String> Test(@PathVariable String name){
-      //       Person p = new Person();
-      //       p.setId("123");
-      //       p.setJob("good");
-      //       p.setName(name);
-      //       personMongoDBRepository.save(p);
-      //       return new ResponseEntity<String>(personMongoDBRepository.findById("500").toString(),HttpStatus.OK);
-      // }
 }
