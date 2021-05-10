864e659cb86cea7a8a7fa98840c4bd6d436fdc16
최규수 <qsoo3810@gmail.com>
2021-05-10 10:36:20 +0900

    feat: 카카오- RN, 토큰 발급, 조회, 프로필 조회

--- frontend/src/screens/Login/LoginScreen.js
+++ frontend/src/screens/Login/LoginScreen.js
@@ -37,57 +37,96 @@ import {
   login,
   logout,
   unlink,
+  // 엑세스 토큰 정보
+  getAccessToken as getKakaoAccessToken,
+  KakaoAccessTokenInfo,
 } from '@react-native-seoul/kakao-login';
 
 function LoginPage({ navigation }) {
-  // state
-  const [result, setResult] = useState('');
 
-  // 로그인
-  const signInWithKakao = async () => {
+  // 카카오 정보 조회
+  const [kakaoInfo, setKakaoInfo] = useState({
+    token: null,
+    profile: null,
+  })
+
+  // 카카오 엑세스 토큰 정보 조회
+  const [kakaoAccessTokenInfo, setKakaoAccessTokenInfo] = useState(null)
+
+  // 실제 로그인
+  const roufarmLogin = async () => {
+    // 1. 토큰 발급
     try {
       const token: KakaoOAuthToken = await login();
-      console.log(token)
-      setResult(JSON.stringify(token));
-      console.log(result, 'here')
-      console.log(token, 'here')
+      setKakaoInfo((prev) => {
+        prev = { ...kakaoInfo, token: token }
+        console.log('받은 토큰 정보', prev)
+        return prev
+      });
     } catch (e) {
-      console.log(e, 'here');
-      console.error(e);
-    }
-  };
-
-  // 로그아웃
-  const signOutWithKakao = async () => {
-    try {
-      const message = await logout();
-      setResult(message);
-    } catch (e) {
+      console.log('카카오 토큰 발급 실패')
       console.error(e)
     }
-  };
-
-  // 프로필 조회
-  const getProfile = async () => {
+    // 2. 프로필 조회
     try {
       const profile: KakaoProfile = await getKakaoProfile();
-      setResult(JSON.stringify(profile));
-      console.log(result, 'here')
-      console.log(profile, 'here')
+      setKakaoInfo((prev) => {
+        prev = { ...kakaoInfo, profile: profile }
+        console.log('받은 프로필 정보', prev)
+        return prev
+      });
     } catch (e) {
+      console.log('카카오 프로필 조회 실패')
       console.error(e)
     }
   };
+  // 카카오 로그인
+  const signInWithKakao = async () => {
+    const token: KakaoOAuthToken = await login();
 
-  // // 연결 끊기
-  // const unlinkKakao = async () => {
-  //   try {
-  //     const message = await unlink();
-  //     setResult(message);
-  //   } catch (e) {
-  //     console.error(e)
-  //   }
-  // };
+    setResult(JSON.stringify(token));
+  };
+  // 카카오 로그아웃
+  const signOutWithKakao = async () => {
+    const message = await logout();
+
+    setKakaoInfo({
+      token: null,
+      profile: null,
+    });
+    setKakaoAccessTokenInfo(null);
+  };
+  // 카카오 프로필 조회
+  const getProfile = async () => {
+    const profile: KakaoProfile = await getKakaoProfile();
+
+    setResult(JSON.stringify(profile));
+  };
+  // 카카오 연결 끊기
+  const unlinkKakao = async () => {
+    const message = await unlink();
+
+    setKakaoInfo({
+      token: null,
+      profile: null,
+    });
+
+    setKakaoAccessTokenInfo(null);
+  };
+  // 카카오 엑세스 토큰 정보 조회
+  const getAccessTokenInfo = async () => {
+    try {
+      const accessTokenInfo: KakaoAccessTokenInfo = await getKakaoAccessToken();
+      setKakaoAccessTokenInfo((prev) => {
+        prev = { ...kakaoAccessTokenInfo, accessTokenInfo }
+        console.log('가지고 있는 엑세스 토큰 정보', prev)
+        return prev
+      })
+    } catch (e) {
+      console.log('엑세스 토큰 정보 조회 에러 발생')
+      console.error(e)
+    }
+  };
 
   return (
     <ScrollView>
@@ -99,13 +138,22 @@ function LoginPage({ navigation }) {
           </AppName>
           <Subtitle>부지런한 농부의 마음으로 시작하는 루틴 관리</Subtitle>
         </Content1>
-        <Text>{result}</Text>
+        <Text>카카오 정보: {JSON.stringify(kakaoInfo)}</Text>
+        <Text>엑세스 토큰 정보: {JSON.stringify(kakaoAccessTokenInfo)}</Text>
         <Content2>
           {/* App Logo */}
           <Logo
             resizeMode={'contain'}
             source={require('../../assets/images/slave1.png')}></Logo>
           {/* kakao login btn */}
+          <Btn onPress={() => roufarmLogin()}>
+            <WithLocalSvg
+              asset={kakaoSymbol}
+              width={15}
+              height={20}
+              fill={'#000000'} />
+            <BtnText>test</BtnText>
+          </Btn>
           <Btn onPress={() => signInWithKakao()}>
             <WithLocalSvg
               asset={kakaoSymbol}
@@ -113,6 +161,15 @@ function LoginPage({ navigation }) {
               height={20}
               fill={'#000000'} />
             <BtnText>카카오 로그인</BtnText>
+          </Btn>
+          {/* 로그아웃 */}
+          <Btn onPress={() => getAccessTokenInfo()}>
+            <WithLocalSvg
+              asset={kakaoSymbol}
+              width={15}
+              height={20}
+              fill={'#000000'} />
+            <BtnText>카카오 토큰 조회</BtnText>
           </Btn>
           {/* 로그아웃 */}
           <Btn onPress={() => signOutWithKakao()}>
@@ -202,4 +259,5 @@ const BtnText = styled.Text`
   margin-left: 15px;
   color: #0f0f0f;
 `;
+
 export default LoginPage;
