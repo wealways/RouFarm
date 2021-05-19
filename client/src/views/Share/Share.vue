b9b27f8c96c020dfe3870c3e30612879e5651e18
최규수 <qsoo3810@gmail.com>
2021-05-19 14:49:02 +0900

    feat: 자랑하기 링크 기본 레이아웃

--- client/src/views/Share/Share.vue
+++ client/src/views/Share/Share.vue
@@ -1,28 +1,40 @@
 <template>
-  <div>
-    test
-    <div>{{ test }}</div>
-    <div>{{ $route.path }}</div>
+  <div id="wrap">
+    <!-- 콤보 정보(header) -->
+    <div id="comb" v-html="headerContent"></div>
+    <!-- 캘린더 컴포넌트가 들어갈 부분 -->
+    <Calendar />
+    <!-- 가입 일자 정보 나올 부분 -->
+    <div id="signupdate" v-html="usingDate"></div>
   </div>
 </template>
 
 <script>
 // axios 직접 쓰면 어떨까
 import axios from "axios";
+import Calendar from "@/components/Calendar";
 
 export default {
   name: "Share",
-  components: {},
+  components: {
+    Calendar,
+  },
   created() {
-    this.apiTest2(this.$route.path);
+    // this.headerContent = this.apiTest2(this.$route.path);
+    console.log(this.headerContent);
   },
   mounted() {
     // this.apiTest2(this.$route.path);
   },
+  computed() {},
   data() {
     return {
-      test: [],
-      test2: [],
+      // 전체 응답 데이터
+      response: {},
+      // 콤보 정보 데이터 기본 틀
+      headerContent: `RouFarm님은 <br>00일 연속으로 <br>루틴을 지켰어요!`,
+      // 가입날짜 정보
+      usingDate: `User님이 RouFarm과 <br>함께 한지 00일 째👏`,
     };
   },
   methods: {
@@ -41,8 +53,8 @@ export default {
         // 테스트용 조회
         console.log("response - get(user/)");
         console.log(response);
-        // data에 바인딩
-        this.test.push(response.data);
+        // 반환
+        return response.data;
       } catch (e) {
         console.error(e);
         console.log("here");
@@ -52,5 +64,22 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+/* 메인 화면 */
+#wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: bisque;
+  text-align: center;
+}
+/* 연속 정보 */
+#comb {
+  background-color: aqua;
+}
+/* 캘린더 */
+/* 가입일자 */
+#signupdate {
+  background-color: blue;
+}
 </style>
