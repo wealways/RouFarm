c85d8be2214e526e6f27748a1ca40b3668120288
최규수 <qsoo3810@gmail.com>
2021-05-20 10:37:00 +0900

    bugfix: 통계 해쉬태그, 비율 절삭

--- frontend/src/components/Report/CustomPieChart.js
+++ frontend/src/components/Report/CustomPieChart.js
@@ -1,15 +1,16 @@
-import React,{useState,useContext,useEffect} from 'react';
-import { StyleSheet, View,Text,useWindowDimensions } from "react-native";
-import { VictoryPie, VictoryLegend,VictoryLabel } from "victory-native";
+import React, { useState, useContext, useEffect } from 'react';
+import { StyleSheet, View, Text, useWindowDimensions } from "react-native";
+import { VictoryPie, VictoryLegend, VictoryLabel } from "victory-native";
 import Svg from 'react-native-svg';
 
 import HeatmapContext from '@/contexts/Report/Heatmap';
 
 
-const CustomPieChart = ({date,res}) => {
+const CustomPieChart = ({ date, res }) => {
+  console.log(res, 'res')
   const height = useWindowDimensions().height;
-  const {heatmap,pieClickDispatch} = useContext(HeatmapContext);
-  const [myData,setMyData] = useState([])
+  const { heatmap, pieClickDispatch } = useContext(HeatmapContext);
+  const [myData, setMyData] = useState([])
   const HashTagData = [
     { x: "운동", y: 0 },
     { x: "지식", y: 0 },
@@ -17,56 +18,56 @@ const CustomPieChart = ({date,res}) => {
     { x: "기타", y: 0 },
   ];
 
-  useEffect(()=>{
-    if(res[heatmap.date]!==undefined){
+  useEffect(() => {
+    if (res[heatmap.date] !== undefined) {
       const temp = res[heatmap.date]['해쉬태그별']
       Object.keys(temp)
-      .forEach(i=>{
-        temp[i].forEach(j=>{
-          if(i==='운동') HashTagData[0]['y']+=j['cnt']
-          else if(i==='지식') HashTagData[1]['y']+=j['cnt']
-          else if(i==='자기개발') HashTagData[2]['y']+=j['cnt']
-          else HashTagData[3]['y']+=j['cnt']
-  
+        .forEach(i => {
+          temp[i].forEach(j => {
+            if (i === '운동') HashTagData[0]['y'] += j['cnt']
+            else if (i === '지식') HashTagData[1]['y'] += j['cnt']
+            else if (i === '자기개발') HashTagData[2]['y'] += j['cnt']
+            else HashTagData[3]['y'] += j['cnt']
+
+          })
         })
-      })
-  
-      const myData = HashTagData.map((val)=>{
-        return {x:val['x'],y:val['y']}
+
+      const myData = HashTagData.map((val) => {
+        return { x: val['x'], y: val['y'] }
       })
       setMyData(myData)
     }
-  },[heatmap.date])
-  
-  
-  
+  }, [heatmap.date])
+
+
+
   let month
-  if(heatmap.date===''){
-    month = parseInt(date[0].split('-')[1])+'월'
-  }else{
-    month = parseInt(heatmap.date.split('-')[1])+'월'
+  if (heatmap.date === '') {
+    month = parseInt(date[0].split('-')[1]) + '월'
+  } else {
+    month = parseInt(heatmap.date.split('-')[1]) + '월'
   }
   const width = useWindowDimensions().width;
 
   return (
-    <View style={{marginLeft:-25,marginTop:-25,height:height/5*2}}>
+    <View style={{ marginLeft: -25, marginTop: -25, height: height / 5 * 2 }}>
       <Svg>
         <VictoryLegend
-          x = {60}
-          y = {10}
+          x={60}
+          y={10}
           gutter={30}
           orientation="horizontal"
           // style={{ border: { stroke: "black" } }}
           // colorScale={["#6f95aa", "#0c985e","#dce8ef","#687396" ]}
-          colorScale={["#DE9E9B", "#7EC07A","#86C5C9","#E75B46" ]}
+          colorScale={["#DE9E9B", "#7EC07A", "#86C5C9", "#E75B46"]}
           data={[
-            { name: "운동" }, { name: "지식" },{ name: "자기개발" }, { name: "기타" }
+            { name: "운동" }, { name: "지식" }, { name: "자기개발" }, { name: "기타" }
           ]}
         />
-        <VictoryPie 
+        <VictoryPie
           innerRadius={50}
           // colorScale={["#6f95aa", "#0c985e","#dce8ef","#687396" ]}
-          colorScale={["#DE9E9B", "#7EC07A","#86C5C9","#E75B46" ]}
+          colorScale={["#DE9E9B", "#7EC07A", "#86C5C9", "#E75B46"]}
           animate={{
             duration: 1500,
             onLoad: { duration: 1000 }
@@ -74,7 +75,7 @@ const CustomPieChart = ({date,res}) => {
           domainPadding={{ x: [0, 100] }}
           data={myData}
           height={350}
-          style={{ labels: { fill: "black", fontSize: 0 }, data:{opacity:0.8}}}
+          style={{ labels: { fill: "black", fontSize: 0 }, data: { opacity: 0.8 } }}
           labelRadius={80}
           // labels={({ datum }) => ``}
           events={[{
@@ -85,7 +86,7 @@ const CustomPieChart = ({date,res}) => {
                   {
                     target: "data",
                     mutation: ({ style }) => {
-                      return  { style: { opacity: 0.6 } };
+                      return { style: { opacity: 0.6 } };
                     }
                   }
                 ];
@@ -93,15 +94,15 @@ const CustomPieChart = ({date,res}) => {
               onPressOut: () => {
                 return [
                   {
-                    target:"data",
-                    mutation: ({style}) => {
+                    target: "data",
+                    mutation: ({ style }) => {
                       return null
                     }
                   }, {
                     target: "labels",
                     mutation: ({ text }) => {
                       pieClickDispatch(text)
-                      return 
+                      return
                     }
                   }
                 ]
